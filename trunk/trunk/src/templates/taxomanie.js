c454430c175dd07be8e34d78bde4d3c25c0a1994
clairon <clairon@0c35f8e9-3944-0410-b44f-6b735169496d>
2008-02-11 16:23:43 +0000

    NP: tentative de querry + images

--- trunk/trunk/src/templates/taxomanie.js
+++ trunk/trunk/src/templates/taxomanie.js
@@ -9,7 +9,7 @@ function getXhr(){
                 xhr = new ActiveXObject("Microsoft.XMLHTTP");
             }
     }
-    else { // XMLHttpRequest non supporté par le navigateur 
+    else {
         alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
         xhr = false; 
     } 
@@ -19,7 +19,7 @@ function getXhr(){
 /**
 * Methode qui sera appelee au survol d'un nom
 */
-function go(){
+function go(taxon){
     var xhr = getXhr()
     // On defini ce qu'on va faire quand on aura la reponse
     xhr.onreadystatechange = function(){
@@ -28,16 +28,14 @@ function go(){
             afficheDescURL(xhr.responseText);
         }
     }
-    xhr.open("GET","essaiAJAX.php",true);
+    xhr.open("GET","getImgUrl?taxon='"+taxon+"'",true);
     xhr.send(null);
 }
-function afficheDescURL(id, toThis){
+function afficheDescURL(toThis){
     if (document.getElementById){
-        document.getElementById(id).innerHTML = toThis;
+        document.getElementById("preview").innerHTML = toThis;
     }
     else if (document.all){
-        document.all[id].innerHTML = toThis;
+        document.all["preview"].innerHTML = toThis;
     }
 }
-//#1234 { position:absolute; z-index:3; top:8px; right:0px; text-align:left; border-style:solid; border-width:thin; background:lightgrey;}
-//<a id="le_nom_du_truc" class="genre" href="http://www.apple.com/fr" onmouseover='go()' onmouseout="afficheDescURL('','')">Pan</a>
