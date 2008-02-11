505e26c9613f5883ea1d391a3757603189b267f0
clairon <clairon@0c35f8e9-3944-0410-b44f-6b735169496d>
2008-02-11 11:31:32 +0000

    Pritable version cliquable + affichage des treecollections

--- trunk/src/taxomanie.py
+++ trunk/src/taxomanie.py
@@ -9,9 +9,11 @@ from cherrypy.lib import static
 
 from lib import phylogelib
 from phylogenictree import PhylogenicTree
-from pleet.pleet import Pleet
+from treecollection import TreeCollection
+from taxobject import Taxobject 
+# from pleet.pleet import Pleet
 
-class Taxomanie(object):
+class Taxomanie( Taxobject ):
     
     reference = None
 
@@ -19,20 +21,19 @@ class Taxomanie(object):
         if self.reference is None:
             from referencetree import ReferenceTree
             Taxomanie.reference = ReferenceTree()
-        self.pleet = Pleet()
         self.collection = []
         self.named_tree = {}
         self.path = "templates/"
 
-    def _presentation( self, file_name, error="" ):
-        full_path = os.path.join( self.path, file_name )
-        ext = os.path.splitext( file_name )[1]
-#        if ext == ".pyhtml":
-        self.pleet.setTemplate( open(full_path).read() )
-        self.pleet["error"] = error
-        return self.pleet.render()
-#        elif ext in ( ".html", ".htm", ".xhtml" ) :
-#            return open( full_path ).read() 
+#    def _presentation( self, file_name, error="" ):
+#        full_path = os.path.join( self.path, file_name )
+#        ext = os.path.splitext( file_name )[1]
+##        if ext == ".pyhtml":
+#        self.pleet.setTemplate( open(full_path).read() )
+#        self.pleet["error"] = error
+#        return self.pleet.render()
+##        elif ext in ( ".html", ".htm", ".xhtml" ) :
+##            return open( full_path ).read() 
 #        else:
 #            raise TypeError, "Wrong template type %s" % file_name
 
@@ -41,47 +42,39 @@ class Taxomanie(object):
         return open( "templates/site.css" ).read()
 
     @cherrypy.expose
-    def index( self, error = "" ):
-        return self._presentation( "index.html" )
+    def index( self, msg = "" ):
+        return self._presentation( "index.html", msg )
     
     @cherrypy.expose
-    def check( self, myFile=None, index=1, target="html" ):
+    def check( self, myFile=None, index=1, target="text" ):
         # return PhylogenicTree( myFile, self.reference ).display("html")
         try:
             if myFile is not None:
                 self.named_tree = {}
                 self.collection = []
                 if isinstance( myFile, str ):
-                    collection = myFile 
+                    input = myFile 
                 else:
                     size = 0
-                    collection = ""
+                    input = ""
                     while True:
                         recv = myFile.file.read(8192)
-                        collection += recv
+                        input += recv
                         if not recv:
                             break
                         size += len(recv)
-                collection = collection.strip()
-                # Nexus collection
-                if collection[:6].lower() == "#nexus":
-                    for tree in collection.split(";")[1:-2]:
-                        tree = tree.strip()
-                        nwktree = tree.split("=")[1]
-                        tree_name = tree.split("=")[0].split()[1]
-                        self.collection.append( nwktree )
-                        self.named_tree[nwktree] = tree_name
-                # Phylip collection
-                else:
-                    self.collection = [col for col in collection.strip().split( ";" ) if col]
-            self.pleet["collection"] = self.collection
-            self.pleet["target"] = target.strip()
-            self.pleet["index"] = int(index)
-            self.pleet["reference"] = self.reference
-            self.pleet["named_tree"] = self.named_tree
+                input = input.strip()
+                try:
+                    self.collection = TreeCollection( input, self.reference )
+                except ValueError, e:
+                    return self._presentation( "index.html", msg = e)
+            print self.collection.collection
+            self._pleet["_collection_"] = self.collection.collection
+            self._pleet["_target_"] = target.strip()
+            self._pleet["_index_"] = int(index)
             return self._presentation( "check.html" )
         except IndexError:
-            return self._presentation( "index.html", error = "No Phylip or Nexus collection found")
+            return self._presentation( "index.html", msg = "No Phylip or Nexus collection found")
 
         
     def download(self):
