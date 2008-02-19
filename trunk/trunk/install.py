6fe8abe250aa1bb1207103e030feedf31403fa8d
clairon <clairon@0c35f8e9-3944-0410-b44f-6b735169496d>
2008-02-19 18:16:58 +0000

    mise a jour de l'install

--- trunk/trunk/install.py
+++ trunk/trunk/install.py
@@ -2,10 +2,12 @@
 
 import os.path
 
+os.system( "mv src/taxomanie.conf ." )
 print "cleaning old version..."
 os.system( "rm -rf src" )
 print "updating new version..."
 os.system( "svn update" )
+os.system( "mv taxomanie.conf src" )
 print "generating taxonomy.csv"
 os.chdir( "src/tools" )
 os.system( "python preproc.py" )
