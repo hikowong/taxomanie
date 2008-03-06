6b904a8a5f82ea4da7276f31df367c24cbd70f58
clairon <clairon@0c35f8e9-3944-0410-b44f-6b735169496d>
2008-03-06 17:41:08 +0000

    Ajout d'un fichier manquant pour la seconde stat... il se fait tard

---
+++ trunk/src/stat2.py
@@ -0,0 +1,17 @@
+from pychart import *
+#theme.get_options()
+theme.output_format = "png"
+#theme.output_format = "x11"
+theme.reinitialize()
+import sys
+
+def stat( data ):
+    ar = area.T( size=(500,500),
+      x_axis= axis.X(label="Taxa"),
+      y_axis = axis.Y(label="Trees number")
+    )
+    ar.add_plot(bar_plot.T(data=data, fill_style = None))
+    a = ar.draw()
+
+a = eval(sys.argv[1])
+stat( a )
