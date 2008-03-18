6093eb625cf03a33e085ca331fae739caf6bbaf1
clairon <clairon@0c35f8e9-3944-0410-b44f-6b735169496d>
2008-03-18 12:59:46 +0000

    restructuration des templates

--- trunk/src/taxobject.py
+++ trunk/src/taxobject.py
@@ -7,8 +7,10 @@ class Taxobject( object ):
     _path_templates = "templates/"
     _pleet = Pleet()
     
-    def _presentation( self, filename, msg="" ):
+    def _presentation( self, filename, msg="", pagedef="" ):
         full_path = os.path.join( self._path_templates, filename )
-        self._pleet.setTemplate( open(full_path).read() )
+        self._pleet.setTemplate(
+          self.header() + open(full_path).read() + self.footer() )
         self._pleet["_msg_"] = msg
+        self._pleet["_pagedef_"] = pagedef
         return self._pleet.render()
