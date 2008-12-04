dfd5b98f2606b4b6f7f37a11030b3db6246d6b7d
clairon <clairon@0c35f8e9-3944-0410-b44f-6b735169496d>
2008-12-04 08:22:29 +0000

    correction bug si mauvais format de collection

--- phylocore-1.0/phylocore/app/djangophylocore/lib/nexus.py
+++ phylocore-1.0/phylocore/app/djangophylocore/lib/nexus.py
@@ -17,7 +17,7 @@ class Nexus( object ):
 
     def __init__( self, nexus_string ):
         # Nexus collection
-        if not nexus_string.lower().strip()[:6] == "#nexus":
+        if not nexus_string.lower().strip()[:6] == "#nexus" or "end;" not in nexus_string.lower():
             raise ValueError, "Wrong nexus string"
         self.__nexus_ori = nexus_string
 #        self.bad_trees_names = self.get_bad_trees_names()
