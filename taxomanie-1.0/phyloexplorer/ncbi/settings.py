4189b729fa7af9f79239d4c6a387e9fb5b5200f6
clairon <clairon@0c35f8e9-3944-0410-b44f-6b735169496d>
2008-12-07 11:10:19 +0000

    cleanup sur settings

--- taxomanie-1.0/phyloexplorer/ncbi/settings.py
+++ taxomanie-1.0/phyloexplorer/ncbi/settings.py
@@ -5,16 +5,6 @@ from phylocore_ncbi.settings import *
 ##############################################################################
 ################# Please edit this section for your needs ####################
 
-#DATABASE_ENGINE = 'sqlite3'           # 'mysql' or 'sqlite3'
-#
-## If you choosed mysql please fill the lines bellow
-#DATABASE_NAME = ''    # Database name
-#DATABASE_USER = ''    # User name
-#DATABASE_PASSWORD = ''     # Password for the user
-## You can also pass optional database informations
-#DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-#DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
 ADMINS = (
     ('John foo', 'john@foo.com'),
 )
@@ -25,7 +15,7 @@ DEBUG = True
 ###############################################################################
 ################################ DON'T EDIT ###################################
 
-TAXONOMY_ENGINE = 'ncbi'            # 'ncbi" or 'itis'
+TAXONOMY_ENGINE = 'ncbi' 
 
 assert TAXONOMY_ENGINE == 'ncbi', "TAXONOMY_ENGINE must be 'ncbi'"
 assert DATABASE_ENGINE in ['mysql','sqlite3'], "DATABASE_ENGINE must be 'mysql' or 'sqlite3'"
@@ -42,21 +32,6 @@ ROOT_PATH = os.path.join(os.getcwd(), os.path.dirname(__file__) )
 
 TEMPLATE_DEBUG = DEBUG
 MANAGERS = ADMINS
-
-#XXX seulement pour ncbi pour l'instant
-
-## For sqlite3
-#if DATABASE_ENGINE == 'sqlite3':
-#    import phylocore_ncbi
-#    DATABASE_PATH = os.path.join( os.path.split( phylocore_ncbi.__file__ )[0] )
-#    DATABASE_NAME = os.path.join( DATABASE_PATH, 'phylocore_%s.db' % TAXONOMY_ENGINE )    # Or path to database file if using sqlite3.
-#elif DATABASE_ENGINE == 'mysql':
-#    pass
-#else:
-#    raise RuntimeError, "Database engine %s not supported" % DATABASE_ENGINE
-#
-TEST_DATABASE_NAME = 'test_phylocore'
-#BOOST_SQLITE = False
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
